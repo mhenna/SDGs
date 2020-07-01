87dfb1ebdb7552f9d1cd3b8490b4d9abd79c2155
karimmady <karimmady5@hotmail.com>
2020-07-01 18:58:04 +0200

    Get discussion (#25)
    * added discussion modal
    
    * added add question endpoint
    
    * added endpoint to answer question in discussion
    
    * added endpoint to return the discussion of a project
    
    Co-authored-by: Mady <K.Mady@dell.com>

--- src/main/java/com/techdev/sdg/Discussion/Discussion.java
+++ src/main/java/com/techdev/sdg/Discussion/Discussion.java
@@ -49,8 +49,10 @@ public class Discussion implements Serializable {
 
     public Discussion(String question) {
         setQuestion(question);
-
     }
+
+    public Long getId() { return this.id; }
+
 
     public void setQuestion(String question) {
         this.question = question;
@@ -91,7 +93,6 @@ public class Discussion implements Serializable {
         discussion.put("id", id);
         discussion.put("question", question);
         discussion.put("answer", answer);
-        discussion.put("project", project);
 
         return discussion;
     }
--- src/main/java/com/techdev/sdg/Discussion/DiscussionRepository.java
+++ src/main/java/com/techdev/sdg/Discussion/DiscussionRepository.java
@@ -1,10 +1,15 @@
 package com.techdev.sdg.Discussion;
 
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface DiscussionRepository extends JpaRepository<Discussion, Long> {
-
+    @EntityGraph(attributePaths  = {"project"})
+    List<Discussion> findByProjectId(Long id);
 }
