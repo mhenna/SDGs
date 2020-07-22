98edf80843353d8064348d64d4e47ca98207faaf
mhenna <mhenna@aucegypt.edu>
2020-07-22 16:20:45 +0200

    added admin approval endpoint and blocked admin endpoints for users

--- src/main/java/com/techdev/sdg/Admin/Router.java
+++ src/main/java/com/techdev/sdg/Admin/Router.java
@@ -1,6 +1,7 @@
 package com.techdev.sdg.Admin;
 
 public class Router {
-    final public static String GETSIGNUPREQUESTS = "admin/signupRequests";
 
+    final public static String GETSIGNUPREQUESTS = "admin/signupRequests";
+    final public static String APPROVESIGNUPREQUEST = "admin/signupRequest/{id}/{type}";
 }
---
+++ src/main/java/com/techdev/sdg/Role.java
@@ -0,0 +1,6 @@
+package com.techdev.sdg;
+
+public class Role {
+    public final static String ADMIN = "admin";
+    public final static String USER = "user";
+}
