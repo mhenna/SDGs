ed2ca76bc1e395b52f7e8dffde0dd73bd9991732
karimmady <karimmady5@hotmail.com>
2020-08-30 19:49:09 +0200

    refactor general forum (#44)
    * refactor general forum
    
    * fixed entity ID bug
    
    Co-authored-by: Mady <K.Mady@dell.com>

--- src/main/java/com/techdev/sdg/GeneralForum/Question/Question.java
+++ src/main/java/com/techdev/sdg/GeneralForum/Question/Question.java
@@ -2,18 +2,25 @@ package com.techdev.sdg.GeneralForum.Question;
 
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.techdev.sdg.GeneralForum.Answer.Answer;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @javax.persistence.Entity
 @Table(name = "Question")
 public class Question implements Serializable {
     final public static String ID = "id";
     final public static String QUESTION = "question";
     final public static String SUBMITTER = "submitter";
+    final public static String TITLE = "title";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,27 +30,53 @@ public class Question implements Serializable {
     @Column(name = "question", nullable = false)
     private String question;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @ManyToOne(optional = false)
     @JoinColumn(name = "entity_id", nullable = false)
     @JsonBackReference
     private com.techdev.sdg.Entity.Entity submitter;
 
+    @OneToMany(mappedBy = "question", fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Set<Answer> answers = new HashSet<>();
+
     public Question(){}
-    public Question(String question) {
+    public Question(String question, String title) {
         setQuestion(question);
+        setTitle(title);
     }
 
     public void setQuestion(String question) {
         this.question = question;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public void setSubmitter(com.techdev.sdg.Entity.Entity submitter) {
         this.submitter = submitter;
+    }
+
+    public void setAnswers(Set<Answer> answers) {
+        this.answers = answers;
     }
 
     public String getQuestion() {
         return question;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Set<Answer> getAnswers() {
+        return answers;
+    }
+
     public com.techdev.sdg.Entity.Entity getSubmitter(com.techdev.sdg.Entity.Entity submitter) {
         return this.submitter;
     }
@@ -55,6 +88,7 @@ public class Question implements Serializable {
                 "\tid: " + id + ",\n" +
                 "\tquestion: " + question + ",\n" +
                 "\tsubmitter: " + submitter + ",\n" +
+                "\ttitle: " + title + ",\n" +
                 '}';
     }
 
@@ -63,6 +97,7 @@ public class Question implements Serializable {
         Question.put("id", id);
         Question.put("question", question);
         Question.put("submitter", submitter);
+        Question.put("title", title);
 
 
         return Question;
--- src/main/java/com/techdev/sdg/GeneralForum/Question/QuestionService.java
+++ src/main/java/com/techdev/sdg/GeneralForum/Question/QuestionService.java
@@ -3,6 +3,9 @@ import com.techdev.sdg.Entity.Entity;
 import com.techdev.sdg.Entity.EntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -17,7 +20,8 @@ public class QuestionService {
 
     public Question submit(Map<String, Object> body) {
         Question question = new Question(
-                Objects.toString(body.get(Question.QUESTION), null)
+                Objects.toString(body.get(Question.QUESTION), null),
+                Objects.toString(body.get(Question.TITLE), null)
         );
 
 
@@ -27,6 +31,21 @@ public class QuestionService {
         question.setSubmitter(submitter);
 
         repository.save(question);
+        return question;
+    }
+
+    public List<Map<String, Object>> findAll() {
+        List<Question> questionsList = repository.findAll();
+        List<Map<String, Object>> questions = new ArrayList<>();
+
+        for (Question e : questionsList)
+            questions.add(e.toMap());
+
+        return questions;
+    }
+
+    public Question findByid(Long id) {
+        Question question = repository.findById(id).get();
         return question;
     }
 
